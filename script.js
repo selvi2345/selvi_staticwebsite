36c7da28f190ff062d3261853f1505a836597d3c
selvi2345 <vu.241fa04123@gmail.com>
2026-09-24 09:55:30 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+const menuButton = document.getElementById("menuButton");
+const navbar = document.getElementById("navbar");
+
+menuButton.addEventListener("click", function () {
+
+    navbar.classList.toggle("active");
+
+});
